9b9c6888b710c5a2e707687fb2af05cbc723e8c8
Dustin Morando <dustinmorando@me.com>
2015-02-18 14:52:15 -0500

    made blog mobel with 2 atrobutes

--- a.js
+++ a.js
@@ -1,1 +1,26 @@
+// -------------
+// Models / Collections
+// -------------
 
+// -------------
+// Views / (Presentation / Interation)
+// -------------
+var Blog = Backbone.Model.extend({
+  defaults:{
+    title:'',
+    body: ''
+  }
+});
+
+
+// -------------
+// Router / Application State
+// -------------
+
+// -------------
+// Configuration
+// -------------
+
+// -------------
+// Glue code
+// -------------
